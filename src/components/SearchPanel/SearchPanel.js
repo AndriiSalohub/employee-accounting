86996806bb21bfb2bf62c8e7a10e43b38dc66562
Andrii Salogub <andrejsalogub3@gmail.com>
2022-06-28 20:23:01 +0300

    creat searchPanel component

---
+++ src/components/SearchPanel/SearchPanel.js
@@ -0,0 +1,11 @@
+import "./SearchPanel.css";
+
+export const SearchPanel = () => {
+    return (
+        <input
+            type="text"
+            className="form-control search-input"
+            placeholder="Найти сотрудника"
+        />
+    );
+};
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,10 +1,15 @@
 import { AppInfo } from "../../components/AppInfo/AppInfo";
+import { SearchPanel } from "../../components/SearchPanel/SearchPanel";
 import "./App.css";
 
 export const App = () => {
     return (
         <div className="app">
             <AppInfo />
+
+            <div className="search-panel">
+                <SearchPanel />
+            </div>
         </div>
     );
 };
