0e4d30d27aa8b5f34160019d2920293d404452e5
Andrii Salogub <andrejsalogub3@gmail.com>
2022-06-28 20:38:38 +0300

    create employeesAddForm component

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,6 +2,7 @@ import { AppFilter } from "../../components/AppFilter/AppFilter";
 import { AppInfo } from "../../components/AppInfo/AppInfo";
 import { EmployeesList } from "../../components/EmployeesList/EmployeesList";
 import { SearchPanel } from "../../components/SearchPanel/SearchPanel";
+import { EmployeesAddForm } from "../../components/EmployeesAddForm/EmployeesAddForm";
 import "./App.css";
 
 export const App = () => {
@@ -15,6 +16,8 @@ export const App = () => {
             </div>
 
             <EmployeesList />
+
+            <EmployeesAddForm />
         </div>
     );
 };
