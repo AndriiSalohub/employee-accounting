a11804f15e9b17d438dfe35102f0de053b269cb9
Andrii Salogub <andrejsalogub3@gmail.com>
2022-06-30 10:23:34 +0300

    add seacrch functionality to searchPanel

--- src/components/SearchPanel/SearchPanel.js
+++ src/components/SearchPanel/SearchPanel.js
@@ -1,11 +1,29 @@
+import React from "react";
 import "./SearchPanel.css";
 
-export const SearchPanel = () => {
-    return (
-        <input
-            type="text"
-            className="form-control search-input"
-            placeholder="Найти сотрудника"
-        />
-    );
-};
+export class SearchPanel extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: "",
+        };
+    }
+
+    onUpdateSearch = (e) => {
+        const term = e.target.value;
+        this.setState({ term });
+        this.props.onUpdateSearch(term);
+    };
+
+    render() {
+        return (
+            <input
+                type="text"
+                className="form-control search-input"
+                placeholder="Найти сотрудника"
+                value={this.state.term}
+                onChange={this.onUpdateSearch}
+            />
+        );
+    }
+}
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -35,6 +35,7 @@ export class App extends React.Component {
                     rise: false,
                 },
             ],
+            term: "",
         };
         this.maxId = 4;
     }
@@ -63,37 +64,6 @@ export class App extends React.Component {
         });
     };
 
-    // onToggleIncrease = (id) => {
-    //     // this.setState(({ data }) => {
-    //     //     // const index = data.findIndex((elem) => elem.id === id);
-    //     //     // const old = data[index];
-    //     //     // const newItem = {
-    //     //     //     ...old,
-    //     //     //     increase: !old.increase,
-    //     //     // };
-    //     //     // const newArr = [
-    //     //     //     ...data.slice(0, index),
-    //     //     //     newItem,
-    //     //     //     ...data.slice(index + 1),
-    //     //     // ];
-    //     //     // return {
-    //     //     //     data: newArr,
-    //     //     // };
-    //     // });
-
-    //     this.setState(({ data }) => ({
-    //         data: data.map((item) => {
-    //             if (item.id === id) {
-    //                 return {
-    //                     ...item,
-    //                     increase: !item.increase,
-    //                 };
-    //             }
-    //             return item;
-    //         }),
-    //     }));
-    // };
-
     onToggleProp = (id, prop) => {
         this.setState(({ data }) => ({
             data: data.map((item) => {
@@ -105,22 +75,28 @@ export class App extends React.Component {
         }));
     };
 
-    // onToggleRise = (id) => {
-    //     this.setState(({ data }) => ({
-    //         data: data.map((item) => {
-    //             if (item.id === id) {
-    //                 return { ...item, rise: !item.rise };
-    //             }
-    //             return item;
-    //         }),
-    //     }));
-    // };
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+
+        return items.filter((item) => {
+            return item.name.indexOf(term) > -1;
+        });
+    };
+
+    onUpdateSearch = (term) => {
+        this.setState({ term });
+    };
 
     render() {
+        const { data, term } = this.state;
         const amountOfWorkers = this.state.data.length;
         const increased = this.state.data.filter(
             (item) => item.increase
         ).length;
+        const visibleData = this.searchEmp(data, term);
+
         return (
             <div className="app">
                 <AppInfo
@@ -129,12 +105,12 @@ export class App extends React.Component {
                 />
 
                 <div className="search-panel">
-                    <SearchPanel />
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch} />
                     <AppFilter />
                 </div>
 
                 <EmployeesList
-                    data={this.state.data}
+                    data={visibleData}
                     onDelete={this.deleteItem}
                     onToggleProp={this.onToggleProp}
                 />
